afa3b8f3ebabb88d99e7843eafd6433b70c0985d
Karmasakshi Goyal <karmasakshi@users.noreply.github.com>
2021-02-03 23:42:09 +0400

    Fix typo

--- constants/faunadb.ts
+++ constants/faunadb.ts
@@ -1,6 +1,6 @@
 import * as faunadb from 'faunadb';
 import { KEYS } from '../constants/keys';
 
-export const faunadbClient = new faunadb.Client({ keepAlive: false, secret: KEYS.FAUNDADB_KEY, timeout: 15 })
+export const faunadbClient = new faunadb.Client({ keepAlive: false, secret: KEYS.FAUNADB_KEY, timeout: 15 })
 
 export const faunadbQuery = faunadb.query;
